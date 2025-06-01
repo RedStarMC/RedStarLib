5ed83b16fbfd0968fd1a91a176026dea208231ec
pingguomc <141195321+pingguomc@users.noreply.github.com>
2025-06-02 07:31:40 +0800

    feat: 数据库Debug，错误抛出，构造函数构建

--- src/main/java/top/redstarmc/plugin/redstarlib/impl/ImplLoggerManager.java
+++ src/main/java/top/redstarmc/plugin/redstarlib/impl/ImplLoggerManager.java
@@ -4,8 +4,7 @@ import top.redstarmc.plugin.redstarlib.manager.LoggerManager;
 
 public class ImplLoggerManager extends LoggerManager {
 
-
-    public ImplLoggerManager(String INFO_PREFIX) {
-        super(INFO_PREFIX);
+    public ImplLoggerManager(String INFO_PREFIX, boolean debugMode) {
+        super(INFO_PREFIX, debugMode);
     }
 }
